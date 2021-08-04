53aff6c9f9f7c5732a5f8b94a952ebb53fd9c5cd
Jonathan Machado <jonhy.jona@gmail.com>
2021-08-04 02:29:07 +0200

    Semifinal

--- src/Database.js
+++ src/Database.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 
 
-mongoose.connect('mongodb://localhost/notes-db-app',{
+mongoose.connect('mongodb://localhost:127.0.0.1:27017/notes-db-app',{
     useCreateIndex:true,
     useNewUrlParser:true,
     useFindAndModify:true,
+    useUnifiedTopology: true
 }).then(db => console.log('db is conection complete')).catch(err=>console.error(err));
 
